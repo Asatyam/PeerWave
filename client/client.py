6185cd484bf3611bfbf1637ebf3800d0792e8724
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2025-01-21 01:05:20 +0530

    Add TLS to client and server

--- client/client.py
+++ client/client.py
@@ -2,32 +2,44 @@ import websockets
 import asyncio
 import os
 import json
+import ssl
+import pathlib
 
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
+ssl_context.load_verify_locations(localhost_pem)
 
-async def send_file(ws):
+async def send_file(ws, file_path):
     file_contents = None
-    with open("./test.txt", "rb") as file:
-        file_contents = file.read()
-    await ws.send(file_contents)
-    print("File sent successfully")
+    try:
+        with open(file_path, "rb") as file:
+            file_contents = file.read()
+        await ws.send(file_contents)
+        print("File sent successfully")
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' does not exist.")
+
+
+def get_file_metadata(file_path: str) -> dict:
+    # TODO: Handle scenario if the file does not exist
+    file_name = os.path.basename(file_path)
+    file_size = os.path.getsize(file_path)
+    return {"file_name": file_name, "file_size": file_size}
+
 
 async def ws_client():
     print("WebScoket: Client Connected")
-    url = "ws://127.0.0.1:7890"
-    async with websockets.connect(url) as ws:
-        filepath = 'test.txt'
-        file_name = os.path.basename(filepath)
-        file_size = os.path.getsize(filepath)
-        metadata = {
-            "file_name": file_name,
-            "file_size": file_size
-        }
+    url = "wss://127.0.0.1:7890"
+    async with websockets.connect(url, ssl=ssl_context) as ws:
+        filepath = "test.txt"
+        metadata = get_file_metadata(file_path=filepath)
+
         to_send_metadata = input("Do you wish to send the metadata(y/n)?")
         if to_send_metadata == "y":
             await ws.send(json.dumps(metadata))
             msg = await ws.recv()
             if msg == "Yes":
-                await send_file(ws)
+                await send_file(ws, filepath)
                 print("File Transferred successfully!")
 
 
--- server/main.py
+++ server/main.py
@@ -1,12 +1,17 @@
 import websockets
 import asyncio
-import typing
+import ssl
+import pathlib
 
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
+ssl_context.load_cert_chain(localhost_pem)
 
 async def receive_file(websocket):
     file_recv = await websocket.recv()
     output_file = "output.txt"
     try:
+        # Open the file and write in binary mode
         with open(output_file, "wb") as file:
             file.write(file_recv)
         print(f" File saved as {output_file}")
@@ -15,7 +20,9 @@ async def receive_file(websocket):
 
 
 async def ws_server(websocket):
-    print("WebSocket: Server started")
+    
+    client_address = websocket.remote_address
+    print(f"New client connected: {client_address}")
 
     try:
         while True:
@@ -30,8 +37,8 @@ async def ws_server(websocket):
 
 
 async def main():
-    print("Starting Server :)")
-    async with websockets.serve(ws_server, "localhost", 7890):
+    async with websockets.serve(ws_server, "localhost", 7890, ssl=ssl_context):
+        print("Started Server :)")
         await asyncio.Future()
 
 
