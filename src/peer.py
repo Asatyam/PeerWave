eadbadb727d732a4f2fa252ca948337f9d05f809
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2025-01-22 01:14:17 +0530

    Fix the bug in dual mode to allow client and server to be at diff. ports

--- src/peer.py
+++ src/peer.py
@@ -10,9 +10,17 @@ def parse_cmd_line_args():
         "--file", dest="filepath", default="test.txt", help="name of file to transfer"
     )
     parser.add_argument(
-        "-p",
-        "--port",
-        dest="port",
+        "-cp",
+        "--client-port",
+        dest="client_port",
+        default=7891,
+        help="Configure the port for client ;Default=7891",
+        type=int,
+    )
+    parser.add_argument(
+        "-sp",
+        "--server-port",
+        dest="server_port",
         default=7890,
         help="Specify the port of the server;Default=7890",
         type=int,
@@ -42,18 +50,39 @@ def parse_cmd_line_args():
     return args
 
 
-async def dual_role_mode(port, address, file_path, chunk_size):
-    server_task = asyncio.create_task(start_server(port))
-    await asyncio.sleep(2)
-    client_task = asyncio.create_task(ws_client(address, port, file_path, chunk_size))
-    await asyncio.gather(server_task, client_task)
 
+#FOR TESTING: As the second instance will be run after a time, the
+# client retries to connect to the server at different port
+async def client_with_retries(
+    address, port, file_path, chunk_size, max_retries=10, delay=2
+):
+    retries = 0
+    while retries < max_retries:
+        try:
+            print(
+                f"Attempting to connect to server at {address}:{port} (try {retries + 1}/{max_retries})"
+            )
+            await ws_client(address, port, file_path, chunk_size)
+            print("Connected successfully!")
+            return  
+        except ConnectionRefusedError:
+            retries += 1
+            print(f"Connection failed. Retrying in {delay} seconds...")
+            await asyncio.sleep(delay)
+
+    print("Max retries reached. Could not connect to the server.")
+
+async def dual_role_mode(server_port,client_port, address, file_path, chunk_size):
+    server_task = asyncio.create_task(start_server(server_port))
+    await asyncio.sleep(2)
+    client_task = asyncio.create_task(client_with_retries(address, client_port, file_path, chunk_size))
+    await asyncio.gather(server_task, client_task)
 
 if __name__ == "__main__":
     args = parse_cmd_line_args()
     if args.role == "server":
-        asyncio.run(start_server(args.port))
+        asyncio.run(start_server(args.server_port))
     elif args.role == "client":
-        asyncio.run(ws_client(args.address, args.port, args.filepath, args.chunk_size))
+        asyncio.run(ws_client(args.address, args.client_port, args.filepath, args.chunk_size))
     elif args.role=="dual":
-        asyncio.run(dual_role_mode(args.port, args.address, args.filepath, args.chunk_size))
+        asyncio.run(dual_role_mode(args.server_port, args.client_port, args.address, args.filepath, args.chunk_size))
